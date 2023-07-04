6506a0e0b1c2bef5488a8644697b989d7dd1d630
zhoooou <52wdwpf@gmail.com>
2023-07-04 11:10:51 +0800

    modify the model.py add a datetime property in the bill class and so on

--- model/model.py
+++ model/model.py
@@ -2,7 +2,7 @@ from DB import db
 from sqlalchemy.sql import func
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_email = db.Column(db.String(50), nullable=False)
     password=db.Column(db.String(20))
@@ -11,66 +11,68 @@ class User(db.Model):
     vip = db.Column(db.Integer, default=0)
     total_cost = db.Column(db.Integer, default=0)
 
-    bills = db.relationship('Bill', back_populates='User')
-    user_log = db.relationship('User_log', back_populates='User')
-    recommendation = db.relationship('Recommendation', back_populates='User')
-    collection = db.relationship("Collect", back_populates='User')
+    bill = db.relationship('Bill', backref='User')
+    user_log = db.relationship('User_log', backref='User')
+    recommendation = db.relationship('Recommendation', backref='User')
+    collection = db.relationship("Collect", backref='User')
 
 class Music(db.Model):
-    __tablename__ = 'musics'
+    __tablename__ = 'music'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     music_name = db.Column(db.String(50), nullable=False)
     music_picture = db.Column(db.String(30))
     lyric = db.Column(db.Text)
     album_name = db.Column(db.String(20))
-    musician_id = db.Column(db.Integer, nullable=False)
+    musician_id = db.Column(db.Integer, db.ForeignKey('music.id'))
 
-    user_log = db.relationship('User_log', back_populates='Music')
-    recommendation = db.relationship('Recommendation', back_populates='Music')
-    collection = db.relationship("Collect", back_populates='Music')
+    user_log = db.relationship('User_log', backref='Music')
+    recommendation = db.relationship('Recommendation', backref='Music')
+    collection = db.relationship("Collect", backref='Music')
 
 class Musician(db.Model):
-    __tablename__ = 'musicians'
+    __tablename__ = 'musician'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     music_name = db.Column(db.String(50), nullable=False)
     music_picture = db.Column(db.String(30))
 
 class Tag(db.Model):
-    __tablename__ = 'tags'
+    __tablename__ = 'tag'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     tag_name = db.Column(db.String(50), nullable=False)
 
 class Bill(db.Model):
-    __tablename__ = 'bills'
+    __tablename__ = 'bill'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     bill_date = db.Column(db.DateTime, default=func.now())
     transaction_amount = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Comment(db.Model):
-    __tablename__ = 'comments'
+    __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    music_id = db.Column(db.Integer, db.ForeignKey('musics.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    music_id = db.Column(db.Integer, db.ForeignKey('music.id'))
     content = db.Column(db.Text)
+    comment_date = db.Column(db.DateTime, default=func.mow())
 
 class Collect(db.Model):
-    __tablename__ = 'collects'
+    __tablename__ = 'collect'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    music_id = db.Column(db.Integer, db.ForeignKey('musics.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    music_id = db.Column(db.Integer, db.ForeignKey('music.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     song_list_name = db.Column(db.String(30), nullable=True)
 
 class User_log(db.Model):
     __tablename__ = 'user_log'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    music_id = db.Column(db.Integer, db.ForeignKey('musics.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    music_id = db.Column(db.Integer, db.ForeignKey('music.id'))
     command_type = db.Column(db.Integer)
-    input_time = db.Column(db.DateTime, default=func.now())
+    log_time = db.Column(db.DateTime, default=func.now())
+
 
 class Recommendation(db.Model):
-    __tablename__ = 'recommendations'
+    __tablename__ = 'recommendation'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    music_id = db.Column(db.Integer, db.ForeignKey('musics.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    music_id = db.Column(db.Integer, db.ForeignKey('music.id'))
